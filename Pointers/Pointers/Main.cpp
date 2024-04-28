3c8920761a3ca5d0bcec485fd326e43631746d3a
Jacob Stewart <stewart.l.jacob@gmail.com>
2024-04-28 04:25:43 -0400

    Pointers

---
+++ Pointers/Pointers/Main.cpp
@@ -0,0 +1,33 @@
+// Pointers target a specific memory address allowing manipulation and control over the target memory space.
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
+
+// STEP THROUGH IN DEBUG TO SEE ACTUAL ALLOCATIONS
+int main() {
+
+	// created in the stack
+	int var = 8;
+	
+	// pulling the memory address from int var.
+	int* ptr = &var;
+
+	// accessing the data at the memory address and modifying its value
+	*ptr = 10;
+
+	LOG(var);
+
+	// requesting 8 bytes of memory on the heap
+	char* buffer = new char[8];
+
+	//fills a block of memory with the data specificed.
+	memset(buffer, 0, 8);
+
+	// creating a pointer to a pointer
+	chat** ptr = &buffer;
+
+	// deleting the memory that was allocated.
+	delete[] buffer;
+
+	std::cin.get();
+}
