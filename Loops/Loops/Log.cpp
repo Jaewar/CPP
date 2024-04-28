5423040588f3b4cbd575ce06261dc4653d3a5d79
Jacob Stewart <stewart.l.jacob@gmail.com>
2024-04-28 04:05:13 -0400

    Loops

---
+++ Loops/Loops/Log.cpp
@@ -0,0 +1,11 @@
+#include "Log.h"
+#include <iostream>
+
+// Author: Jacob Stewart
+// Date: 04/26/2024
+// Purpose: Provides definition(s) for logging to console.
+
+void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
