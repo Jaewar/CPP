7d55d1c695a9182667d62bd44dd43fa8bb5d5503
Jacob Stewart <stewart.l.jacob@gmail.com>
2024-04-28 08:08:34 -0400

    Update Main.cpp
    Log Levels changed to Enum

--- Logging/Logging/Main.cpp
+++ Logging/Logging/Main.cpp
@@ -2,30 +2,33 @@
 
 class Log {
 public:
-	const int LogLevelError = 0;
-	const int LogLevelWarning = 1;
-	const int LogLevelInfo = 2;
+	enum Level {
+		LevelError = 0,
+		LevelWarning = 1,
+		LevelInfo = 2
+	};
+
 private:
 	// m_ means it is a class level member variable.
-	int m_LogLevel = LogLevelInfo;
+	Level m_LogLevel = LevelInfo;
 
 public:
-	void SetLevel(int level) {
+	void SetLevel(Level level) {
 		m_LogLevel = level;
 	}
 
 	void Error(const char* message) {
-		if (m_LogLevel >= LogLevelError)
+		if (m_LogLevel >= LevelError)
 			std::cout << "[ERROR]: " << message << std::endl;
 	}
 
 	void Warn(const char* message) {
-		if (m_LogLevel >= LogLevelWarning)
+		if (m_LogLevel >= LevelWarning)
 			std::cout << "[WARNING]: " << message << std::endl;
 	}
 
 	void Info(const char* message) {
-		if (m_LogLevel >= LogLevelInfo)
+		if (m_LogLevel >= LevelInfo)
 			std::cout << "[INFO]: " << message << std::endl;
 	}
 };
@@ -33,7 +36,7 @@ public:
 int main() {
 
 	Log log;
-	log.SetLevel(log.LogLevelWarning);
+	log.SetLevel(Log::LevelWarning);
 	log.Error("Error");
 	log.Warn("Warn");
 	log.Info("Info");
