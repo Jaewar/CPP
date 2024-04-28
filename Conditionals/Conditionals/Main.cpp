41ede0527a39790273a595b475bb1b516d73e1fd
Jacob Stewart <stewart.l.jacob@gmail.com>
2024-04-28 04:01:12 -0400

    LoopControl

--- Conditionals/Conditionals/Main.cpp
+++ Conditionals/Conditionals/Main.cpp
@@ -1,22 +1,72 @@
 #include <iostream>
 #include "Log.h"
 
+// VISUAL STUDIOS: Format Doc (Ctrl + K, D) Format Selection (Ctrl + K, F)
+
 int main() {
 
-	int x = 5;
+	#pragma region LoopControl
 
-	bool comparisonResult = x == 5;
-
-	if (comparisonResult)
-	{
-		Log("Hello World!");
+	for (int i = 0; i < 5; i++) {
+		if (i % 2 == 0)
+			continue;
+		std::cout << i << std::endl;
 	}
 
-	const char* ptr = nullptr;
-	if (ptr)
-		Log(ptr);
-	else
-		Log("PTR is NULL");
+	for (int i = 0; i < 5; i++) {
+		if ((i + 1) % 2 == 0)
+			break;
+		std::cout << i << std::endl;
+	}
+
+	//for (int i = 0; i < 5; i++) {
+	//	if ((i + 1) % 2 == 0)
+	//		return 0;
+	//	std::cout << i << std::endl;
+	//}
+
+	#pragma endregion
+
+	#pragma region ForLoop
+	/*
+	// Var, Condition, Incremental
+	for (int i = 0; i < 5; i++) {
+		Log("Hello World!");
+	}
+		// Same as above loop
+		int i = 0;
+		bool condition = true;
+
+		for (; condition;) {
+			Log("Hello World!!");
+			i++;
+			if (!(i < 5))
+				condition = false;
+		}
+	*/
+	#pragma endregion
+
+	#pragma region WhileLoop
+	/*
+	int j = 0;
+	// while "Condition"
+	while (j < 5) {
+		Log("Hello World!");
+		j++;
+	}
+	*/
+	#pragma endregion
+
+	#pragma region DoWhile
+	/*
+	int k = 0;
+	do {
+		Log("Hello World!");
+		k++;
+	} while (k < 5);
+	*/
+	#pragma endregion
+
 
 	std::cin.get();
 }
